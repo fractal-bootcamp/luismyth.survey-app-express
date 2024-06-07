2c1da32f10ab4730b16aca24be4bf1fc55d43c0d
yab <hello@factions.ai>
2024-06-07 10:45:36 -0400

    de-noising

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,46 +1,46 @@
-import type { ActionFunction, LoaderFunction, MetaFunction } from "@remix-run/node";
+// import type { ActionFunction, LoaderFunction, MetaFunction } from "@remix-run/node";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
-
-export const loader: LoaderFunction = async () => {
-  // return {message: "Welcome to Remix!" };
-  return {survey: surveys};
-};
+// export const meta: MetaFunction = () => {
+//   return [
+//     { title: "New Remix App" },
+//     { name: "description", content: "Welcome to Remix!" },
+//   ];
+// };
 
 
-// OPTIONAL
-
-enum SurveyFormFields {
-  SurveyName = "surveyName",
-  QuestionOne = "questionOne"
-}
-
-//and then later summon these using SurveyFormFields.SurveyName
+// export const loader: LoaderFunction = async () => {
+//   // return {message: "Welcome to Remix!" };
+//   return {survey: surveys};
+// };
 
 
-export const action: ActionFunction = async ({ request }) => {
-  const data = await request.formData();
-  // console.log(data.get("surveyName"
-  await clientInformation.survey.create({
-    data.get("surveyName")?.toString || "default survey name",
-  })
+// // OPTIONAL
 
-  //Updating data structure
-  surveys.push({
-    // id: surveys.length + "1"
-    // lol this willliterally just add it all as 
-    title: data.typed(surveyName).toString       /// unsure if I got this right, and there's more
-    })
-  ))
+// enum SurveyFormFields {
+//   SurveyName = "surveyName",
+//   QuestionOne = "questionOne"
+// }
+
+// //and then later summon these using SurveyFormFields.SurveyName
 
 
-}
+// export const action: ActionFunction = async ({ request }) => {
+//   const data = await request.formData();
+//   // console.log(data.get("surveyName"
+//   await clientInformation.survey.create({
+//     data.get("surveyName")?.toString || "default survey name",
+//   })
+
+//   //Updating data structure
+//   surveys.push({
+//     // id: surveys.length + "1"
+//     // lol this willliterally just add it all as 
+//     title: data.typed(surveyName).toString       /// unsure if I got this right, and there's more
+//     })
+//   ))
+
+
+// }
 
 export default function Index() {
   return (
