7f6e2d94a18f7dc0e6e38bb0b3a58bf0b4c943a3
yab <hello@factions.ai>
2024-06-08 13:42:35 -0400

    tidying

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -60,84 +60,20 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Survey Orangutan</h1>
+      <div>
+      <Link to="/create" >Create</Link>
+      </div>
       <HowManySurveys surveysLength = {surveys.length} />
 
       {surveys.toReversed().map((survey) => (
         <div key = {survey.id}>
-          <p> {survey.name}</p>
+          <Link to = {`/survey/${survey.id}`}>
+            <b>Survey #{survey.id}:  </b>
+            {survey.name}
+          </Link>
         </div>
       ))}
 
-      {/* <div>
-      <Link to="/create" >Create</Link>
-      </div>
-      <HowManySurveys />
-      <ListSurveys /> */}
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// enum SurveyFormFields {
-//   SurveyName = "surveyName",
-//   QuestionOne = "questionOne",
-// }
-
-// export const meta: MetaFunction = () => {
-//   return [
-//     {title: "Juicy new app"},
-//     { name: "description", content: "Welcome to the new version of the website"}
-//   ]
-// }
-
-
-
-
-
-
-
-
-// export const loader = async ({
-//   params,
-// }: LoaderFunctionArgs) => {
-//   console.log("server side")
-//   return json(
-//     await prismaDatabase.survey.findMany()
-//   )
-// }
-
-
-
-// function ListSurveys() {
-//   const data = useLoaderData<typeof loader>();
-//   const x = [1, 2, 3];
-//   console.log(data);
-//   return(
-//     <ul>
-//       {data.toReversed().map((survey) => (
-//           <li key={survey.id}>
-//             <Link to={`/survey/${survey.id}`}>
-//               {survey.name}
-//             </Link>
-//           </li>
-//       ))}
-//     </ul>
-//   )
-// }
-
-// /// Test Comment
-
-
